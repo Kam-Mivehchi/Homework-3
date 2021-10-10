a94c5446f703e014381832d5caaa1f089a4f1cec
Kamyar Mivehchi <kammivehchi@gmail.com>
2021-10-09 23:25:54 -0700

    First Attempt

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Homework-3
+
+
+-created a password generator that gives a user the ability to set specific parameters
+-created function to ask for user length via prompt function, convert that length into an integer.
+- asked the user a series of yes or no questions to define the parameters of the Character set with the confirm function.
+- users answers were used as conditinals to create an acceptable character set.
+- created input validation criteria for which invalid password lengths or the denial of all criteria would return an alert statement and display a message to remedy the error.
+-used for loop to select a random character from the character set for the desired length of the users password
+-joined the array items to return a string if user input is valid.
+-this string is then called by the starter code in the write password function and a password is genrated in the text field
+
+Links
+
---
+++ script.js
@@ -0,0 +1,112 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+//function to generate a random string
+function generatePassword() {
+  var myPassword = [];
+  var lowerAlph= "abcdefghijklmnopqrstuvwxyz".split(",");
+  var upperAlph= "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(",");
+  var number ="1234567890".split(",");
+  var Symbols = "!@#$%^&*()_+=-}{][|~`><?".split(",");
+  var characterSet= lowerAlph+upperAlph+number+Symbols;
+
+
+  //prompt user for password length and convert that input to integer
+  var passLength = prompt("how many charactrs would you like in your password(8-120 max)", "Enter Value here");
+  var passLengthValue= parseInt(passLength);
+
+  //validate input
+  if(passLengthValue>7 &&passLengthValue<121){
+    var wantCap = confirm("Press OK if you would like Capital letters in your password");
+    var wantLower =confirm("Press OK if you would like lowers letters in your password");
+    var wantNumbers =confirm("Press OK if you would like Numbers in your password");
+    var wantSymbols =confirm("Press OK if you would like Symbols in your password");
+    
+    //Input Validation if user denies all characters
+    if(!wantCap&&!wantSymbols&&!wantLower&&!wantNumbers){
+      alert("Please Accept at least one criteria");
+      return "Please Accept at least one criteria";
+    }
+    // if at least one condition is accepted the following will create a character set based on user criteria
+    else if(wantCap&&!wantSymbols&&!wantLower&&!wantNumbers){
+      characterSet = upperAlph;
+    }
+    else if(!wantCap&&wantSymbols&&!wantLower&&!wantNumbers){
+      characterSet = Symbols;
+    }
+    else if(!wantCap&&!wantSymbols&&wantLower&&!wantNumbers){
+      characterSet = lowerAlph;
+    }
+    else if(!wantCap&&!wantSymbols&&!wantLower&&wantNumbers){
+      characterSet = number;
+    }
+    else if(!wantCap&&wantSymbols&&wantLower&&wantNumbers){
+      characterSet = Symbols+ lowerAlph+ number;
+    }
+    else if(wantCap&&!wantSymbols&&wantLower&&wantNumbers){
+      characterSet = upperAlph+ lowerAlph+ number;
+    }
+    else if(wantCap&&wantSymbols&&!wantLower&&wantNumbers){
+      characterSet = Symbols+ upperAlph+ number;
+    }
+    else if(wantCap&&wantSymbols&&wantLower&&!wantNumbers){
+      characterSet = Symbols+ upperAlph+ upperAlph;
+    }
+    else if(!wantCap&&!wantSymbols&&wantLower&&wantNumbers){
+      characterSet = lowerAlph+ number;
+    }
+    else if(!wantCap&&wantSymbols&&!wantLower&&wantNumbers){
+      characterSet = Symbols+ number;
+    }
+    else if(!wantCap&&wantSymbols&&wantLower&&!wantNumbers){
+      characterSet = Symbols+ lowerAlph;
+    }
+    else if(wantCap&&!wantSymbols&&!wantLower&&wantNumbers){
+      characterSet = numbers+upperAlph;
+    }
+    else if(wantCap&&!wantSymbols&&wantLower&&!wantNumbers){
+      characterSet = lowerAlph+upperAlph;
+    }
+    else if(wantCap&&wantSymbols&&!wantLower&&!wantNumbers){
+      characterSet = Symbols+upperAlph;
+    }
+    else if(wantCap&&!wantSymbols&&!wantLower&&!wantNumbers){
+      characterSet = upperAlph;
+    }
+    else if(!wantCap&&wantSymbols&&!wantLower&&!wantNumbers){
+      characterSet = Symbols;
+    }
+    else if(!wantCap&&!wantSymbols&&wantLower&&!wantNumbers){
+      characterSet = lowerAlph;
+    }
+    else if(!wantCap&&!wantSymbols&&!wantLower&&wantNumbers){
+      characterSet = number;
+    }
+
+    //retrieves random character and appends to password
+    for (var i = 0; i<passLengthValue; i++){
+      myPassword[i]= characterSet[Math.floor(Math.random() * characterSet.length)];
+      
+    }
+    //join array as a string without commas, return that string
+    var passString = myPassword.join("");
+    return passString;
+  }
+  //if input is not valid the function returns to screen and tells user to enter a valid function
+  else{
+    alert("Please Enter a Value between 8-120");
+    return "Please Enter a Value between 8-120";
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
